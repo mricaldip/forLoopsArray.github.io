68543905591a67258ae56ee39cdb4f9688288791
Mauricio Ricaldi <76887915+mricaldip@users.noreply.github.com>
2023-05-08 18:12:56 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const input = 'I want to break free!'
+const vowels = ['a', 'e', 'i', 'o', 'u']
+const resultArray = []
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  //console.log(`inputIndex is ${inputIndex}.`)
+  if (input[inputIndex] === 'e'){
+    resultArray.push(input[inputIndex])
+  }
+   if (input[inputIndex] === 'u'){
+    resultArray.push(input[inputIndex])
+  }
+  for (let vowelIndex = 0; vowelIndex < vowels.length; vowelIndex++){
+    //console.log(`vowelIndex is ${vowelIndex}`)
+    if(input[inputIndex] === vowels[vowelIndex]){
+      //console.log(input[inputIndex])
+      resultArray.push(input[inputIndex]);
+    }
+  }
+}
+
+//console.log(resultArray)
+
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString)
